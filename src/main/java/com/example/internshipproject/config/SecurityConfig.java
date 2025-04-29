fc1a61286951d51d6e0a4ff3095bddad8ff52975
Chanchal Saini <chanchalsaini3103@gmail.com>
2025-04-29 11:52:12 +0530

    Initial commit

--- src/main/java/com/example/internshipproject/config/SecurityConfig.java
+++ src/main/java/com/example/internshipproject/config/SecurityConfig.java
@@ -1,3 +1,4 @@
+
 package com.example.internshipproject.config;
 
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/example/internshipproject/controller/AuthController.java
+++ src/main/java/com/example/internshipproject/controller/AuthController.java
@@ -1,3 +1,4 @@
+
 package com.example.internshipproject.controller;
 
 import java.util.Map;
